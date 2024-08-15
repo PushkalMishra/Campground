ed9fbf3807b5394c7c07a7cce9af45f9530397b9
PushkalMishra <pushkalmishra214@gmail.com>
2024-08-16 01:14:01 +0530

    add comment in auth

--- models/user.js
+++ models/user.js
@@ -12,4 +12,9 @@ const UserSchema = new Schema({
 
 UserSchema.plugin(passportLocalMongoose);
 
+// Passport-Local Mongoose is a Monvgoose plugin that simplifies building username and password login with Passport.
+// You're free to define your User how you like. Passport-Local Mongoose will add a username, hash and salt field to store the username, the hashed password and the salt value.
+
+// Additionally, Passport-Local Mongoose adds some methods to your Schema.
+
 module.exports = mongoose.model('User', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,14 @@ router.post('/register', catchAsync(async (req, res, next) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
+        // This is a method provided by the User model, which is likely using a library like passport-local-mongoose for handling user authentication.
+        // The register method takes two arguments:
+        // user: An instance of the User model containing user details like email and username.
+        // password: The plain text password provided by the user during registration.
+        // it store the user instance and the hashed password in the db.
         req.login(registeredUser, err => {
+            // The req.login method is used to establish a login session for the user. It is part of the Passport.js library, which simplifies authentication in Node.js applications.
+            // When req.login is called, Passport.js serializes the user information and stores it in the session. This is done using the serializeUser function that you define when configuring Passport.js.
             if (err) return next(err);
             req.flash('success', 'Welcome to Yelp Camp!');
             res.redirect('/campgrounds');
@@ -30,6 +37,7 @@ router.get('/login',(req,res)=>{
 })
 
 router.post('/login',storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    // passport.authenticate('local', { ... }): Uses Passport.js to authenticate the user with the ‘local’ strategy (typically username and password).
     req.flash('success', 'welcome back!');
     const redirectUrl = res.locals.returnTo||'/campgrounds';
     res.redirect(redirectUrl);
