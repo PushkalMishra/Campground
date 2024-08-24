5ebf1b92b82e70dfc0e3a6f900f4f489d3aed5d3
PushkalMishra <pushkalmishra214@gmail.com>
2024-08-24 13:58:32 +0530

    hi

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,17 @@ const Review = require('./review');
 const { urlencoded } = require('express');
 const Schema = mongoose.Schema;
 
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [
